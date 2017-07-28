34510c618255c7c0479d74137a8d8d92a8966b8a
david <david@DESKTOP-3TB3NBJ>
2017-07-28 07:06:04 -0400

    Wrote logic for reading in IRC log files.

--- src/wordart/Main.java
+++ src/wordart/Main.java
@@ -3,11 +3,24 @@ package wordart;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 	
+	final static String SENTINEL_VALUE = "<luna_moona> !stopWordArt"; // Feel free to change this to your liking.
+	final static String IRC_MESSAGE_INDICATOR = "<"; // This is a value that is in front of all IRC user message,
+													// and will not be mistaken for server messages.
+													// Feel free to change this for your own IRC client.
+	
 	public static void main(String[] args) {
+		boolean exited = false;
+		boolean printedCurMsg = false;
+		BufferedReader br;
 		Robot r;
+		String curMsg, nextMsg, prevMsg, channel, path;
 		
 		try {
 			r = new Robot();
@@ -16,11 +29,67 @@ public class Main {
 			e.printStackTrace();
 		}
 		
+		curMsg = "";
+		nextMsg = "";
+		prevMsg = "";
+		channel = "#monotonetim";
+		
+		path = System.getenv("APPDATA");
+		path += "\\mIRC\\logs\\" + channel + ".log";
+		while (!exited) {
+			try {
+				br = new BufferedReader(new FileReader(path));
+				System.out.println("Reading from " + path);
+				while (nextMsg != null) {
+					curMsg = nextMsg;
+					nextMsg = br.readLine();
+				}
+				
+				if (!(prevMsg.equals(curMsg))
+						&& prevMsg.startsWith(IRC_MESSAGE_INDICATOR)
+						&& !(curMsg.equals(SENTINEL_VALUE))) {
+					
+				} else if (curMsg.equals(SENTINEL_VALUE)) {
+					exited = true;
+				} else {
+					System.out.println("prevMsg is the same as curMsg, not printing out");
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("Unable to read from " + path);
+				e.printStackTrace();
+			} catch (IOException e) {
+				System.out.println("IOException while reading from" + path);
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+			
+			if (printedCurMsg) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					System.out.println("Thread.sleep(1000) interrupted!");
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					System.out.println("Thread.sleep(1000) interrupted!");
+					e.printStackTrace();
+				}
+			}
+		}
+		
 		/*r.mouseMove(200, 400);
 		r.mousePress(InputEvent.BUTTON1_MASK); //proof of concept test code
 		r.mouseRelease(InputEvent.BUTTON1_MASK);*/
 		
 		// loop
+		
+		
 		
 		// poll for log file, get last message
 			// make in separate method that returns string
@@ -41,11 +110,5 @@ public class Main {
 			/// put into separate method
 		
 		// sleep 100 ms
-		
-		// 
-		
-		
-		
-		
 	}
 }
