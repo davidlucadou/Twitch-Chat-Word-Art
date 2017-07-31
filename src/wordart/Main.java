8878b25e06741aac76990090e5b93d2625344e9c
david <david@DESKTOP-3TB3NBJ>
2017-07-31 16:36:01 -0400

    Expanded range of x-targets.

--- src/wordart/Main.java
+++ src/wordart/Main.java
@@ -202,8 +202,8 @@ public class Main {
 		if (prevMsg.equalsIgnoreCase("")) {
 			return;
 		} else {
-			int[] xVals = {640, 650, 655, 660};
-			for (int x = 0; x < xVals.length; x++) {
+			int[] xVals = {640, 650, 655, 658, 660};
+			for (int x = 640; x < 680; x++) {
 				for (int y = 240; y < 300; y+= 2) {
 					r.mouseMove(xVals[x], y);
 					r.mousePress(InputEvent.BUTTON1_MASK);
